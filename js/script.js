26965bff35a7e87bfb38e814cb12c1cfcab1fd9b
Mohamed-Ali5555 <mohammedelbalshy4@gmail.com>
2022-05-10 12:27:52 +0200

    cv portfolio1

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// ===================================typing animaion
+
+var typed = new Typed(".typing",{
+    strings:["","Web Designer" , "Web Developer" , "Front End Develober" , "Back end Develober"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
